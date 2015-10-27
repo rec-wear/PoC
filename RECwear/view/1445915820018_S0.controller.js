375f5398dda0c8f35faf99d711f5575e9e6b815b
byted <stefan.selent@mailbox.com>
2015-10-27 17:08:49 +0000

    filter working

--- RECwear/view/1445915820018_S0.controller.js
+++ RECwear/view/1445915820018_S0.controller.js
@@ -6,6 +6,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(BaseController) {
 		onInit: function() {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("1445915820018_S0").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			
+			var oModel = new sap.ui.model.json.JSONModel({
+				sex: "",
+				hasClub: "FALSE",
+				firstRec: ""
+			});
+			this.getView().setModel(oModel, 'search');
 		},
 		handleRouteMatched: function(oEvent) {
 			var params = {};
@@ -21,6 +28,23 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(BaseController) {
 
 			}
 
-		}
+		},
+        applyFilter: function(oEvent) {
+        	var cb = oEvent.getSource();
+        	var oModel = cb.getModel('search');
+        	var filters = oModel.getData();
+        	
+        	var details = this.oView.oParent.oParent.getCurrentDetailPage();
+			var table = details.mAggregations.content[0].mAggregations.content[0];
+			
+			var oBinding = table.getBinding('items');
+			
+			var oFilterList = [];
+			jQuery.each(filters, function(k, v) {
+				oFilterList.push(new sap.ui.model.Filter(k, sap.ui.model.FilterOperator.Contains, v));
+			});
+			
+			oBinding.filter(oFilterList);
+        }
 	});
 }, /* bExport= */ true);
